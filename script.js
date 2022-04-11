42bbc73e06a2a8f0744a874f0d6d90cc25e5c21a
luca-gif <luca.89m@gmail.com>
2022-04-11 18:50:41 +0200

    Primo esercizio

---
+++ script.js
@@ -0,0 +1,22 @@
+const emailUtente = prompt("Digita la tua email");
+
+const utentiAutorizzati = [
+    "luca@gmail.com",
+    "marco@hotmail.com",
+    "pippo@gmail.com",
+    "sara@gmail.com",
+];
+
+const email = document.querySelector("#email");
+
+let flag = 0;
+
+for (let i = 0; i < utentiAutorizzati.length; i++) {
+    if (emailUtente === utentiAutorizzati[i]) {
+        flag = 1;
+    }
+}
+
+if (flag === 1) {
+    email.innerHTML = "La tua mail è idonea";
+} else email.innerHTML = "La tua mail non è idonea";
