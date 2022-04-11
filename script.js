3c66787644248ac9c2b0b9d4bf3fdc5155a7f0f2
luca-gif <luca.89m@gmail.com>
2022-04-11 19:01:18 +0200

    aggiungo css

--- script.js
+++ script.js
@@ -1,22 +1,24 @@
-const emailUtente = prompt("Digita la tua email");
+document.getElementById("btn").addEventListener("click", function() {
+    const emailUtente = prompt("Digita la tua email");
 
-const utentiAutorizzati = [
-    "luca@gmail.com",
-    "marco@hotmail.com",
-    "pippo@gmail.com",
-    "sara@gmail.com",
-];
+    const utentiAutorizzati = [
+        "luca@gmail.com",
+        "marco@hotmail.com",
+        "pippo@gmail.com",
+        "sara@gmail.com",
+    ];
 
-const email = document.querySelector("#email");
+    const email = document.querySelector("#email");
 
-let flag = 0;
+    let flag = 0;
 
-for (let i = 0; i < utentiAutorizzati.length; i++) {
-    if (emailUtente === utentiAutorizzati[i]) {
-        flag = 1;
+    for (let i = 0; i < utentiAutorizzati.length; i++) {
+        if (emailUtente === utentiAutorizzati[i]) {
+            flag = 1;
+        }
     }
-}
 
-if (flag === 1) {
-    email.innerHTML = "La tua mail è idonea";
-} else email.innerHTML = "La tua mail non è idonea";
+    if (flag === 1) {
+        email.innerHTML = "La tua mail è idonea";
+    } else email.innerHTML = "La tua mail non è idonea";
+});
